d3d262ad5df073352d352f55614432a8f1f01a4a
elflee77 <elflee77@gmail.com>
2022-11-28 15:40:28 +0900

    test

--- vue_tutorial/2048/js/main.js
+++ vue_tutorial/2048/js/main.js
@@ -1,3 +1,84 @@
+class BlockMap{
+    constructor(row,column){
+        this.Row = row;
+        this.Column = column;
+        this.Map = Array.from(Array(row), ()=> Array(column).fill(0));
+        this.div_app = document.getElementById("app");        
+    }
+    addblock(x,y,block){
+        block.setactive(true);
+        block.setcoord(x,y);
+        this.Map[x][y] = block;
+        block.setcoord(x,y);
+        
+        this.div_app.appendChild(block.div_element)
+    }
+    addrandomblock(block){
+        block.setactive(true);
+        
+        
+        //랜덤 블록 위치 생성
+
+
+        block.setcoord(x,y);
+        this.Map[x][y] = block;
+        this.div_app.appendChild(block.div_element)
+    }
+    removeblock(x,y){
+        block.setactive(false);
+        this.Map[x][y] = null;
+    }
+    moveleft(){
+        
+    }
+    moveright(){
+
+    }
+    moveup(){
+
+    }
+    movedown(){
+
+    } 
+
+    draw(){
+        //for(let i=0;i<this.Row;i++)
+        //    for(let j=0;j<this.Column;j++)
+        
+        //this.Map[x][y]
+    }
+}
+
+class Block{
+    constructor(value){
+        this.Value = value;
+        this.Active = false;
+        this.XPos = 20;
+        this.YPos = 20;
+        this.div_element = document.createElement("div");
+        this.div_element.setAttribute("class","blockPrefab")
+        this.div_element.appendChild(document.createTextNode("2"));
+    }
+    changevalue(value){
+        this.Value = value;
+    }
+    setactive(isActivated){
+        this.Active = isActivated;        
+    }
+    setcoord(x,y){
+        this.XPos += 150*x;
+        this.YPos += 150*y;
+        this.div_element.style.left = this.XPos;
+        this.div_element.style.top = this.YPos;
+    }
+    removeblock(){
+        this.Active = false;
+        this.XPos = 20;
+        this.YPos = 20;
+    }
+
+}
+/*
 function keyArrowRight(){
     console.log("Right Pressed.");
     let blocks = document.getElementsByClassName("blockPrefab");
@@ -27,15 +108,12 @@ function keyArrowDown(){
     for (let i = 0; i < blocks.length; i++){
         blocks[i].style.top += 170;        
     }
-}
+}*/
 
-function gameStart(){
-    var blockmap = new BlockMap();
-    blockmap.addblock(new Block(2));
+var blockmap = new BlockMap(4,4);
 
-    
-    document.createElement("div");
-    div_app.appendChild();
+function gameStart(){    
+    blockmap.addblock(Math.floor(Math.random() * 4),Math.floor(Math.random() * 4),new Block(2));
 }
 
 function keylog(e){
@@ -45,16 +123,16 @@ function keylog(e){
             gameStart();
             break;
         case 'ArrowRight':
-            keyArrowRight();
+            blockmap.moveright();
             break;
         case 'ArrowLeft':
-            keyArrowLeft();
+            blockmap.moveleft();
             break;
         case 'ArrowUp':
-            keyArrowUp();
+            blockmap.moveup();
             break;
         case 'ArrowDown':
-            keyArrowDown();
+            blockmap.movedown();
             break;
         default:            
             break;
@@ -63,49 +141,4 @@ function keylog(e){
 
 window.onkeydown = keylog;
 
-class BlockMap{
-    constructor(row,column){
-        this.Map = Array.from(Array(row), ()=> Array(column).fill(0));
-        this.div_app = document.getElementById("app");
-    }
-    addblock(x,y,block){
-        block.setactive(true);
-        block.setcoord(x,y);
-        this.Map[x][y] = block;
-    }
-    removeblock(x,y){
-        block.setactive(false);
-        this.Map[x][y] = null;
-    } 
 
-}
-
-class Block{
-    constructor(value){
-        this.Value = value;
-        this.Active = false;
-        this.XPos = 20;
-        this.YPos = 20;
-        this.Element = document.createElement("div");
-        this.Element.setAttribute("class","blockPrefab")
-    }
-    changevalue(value){
-        this.Value = value;
-    }
-    setactive(isActivated){
-        this.Active = isActivated;        
-    }
-    setcoord(x,y){
-        this.XPos += 150*x;
-        this.YPos += 150*y;
-    }
-    removeblock(){
-        this.Active = false;
-        this.XPos = 20;
-        this.YPos = 20;
-    }
-
-}
-
-blockMap[0][0] = new Block(2);
-blockMap[0][3] = new Block(2);
