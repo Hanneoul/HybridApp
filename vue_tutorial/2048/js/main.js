d423a232734788ccb88eac30dad435869eb48503
elflee77 <elflee77@gmail.com>
2022-11-28 11:46:52 +0900

    mid raw

--- vue_tutorial/2048/js/main.js
+++ vue_tutorial/2048/js/main.js
@@ -2,18 +2,8 @@ function keyArrowRight(){
     console.log("Right Pressed.");
     let blocks = document.getElementsByClassName("blockPrefab");
     for (let i = 0; i < blocks.length; i++){
-        blocks[i].style.left = 20;
         console.log(blocks[i].style.left);
-        if(blocks[i].style.left == '20px')
-        {
-            //이찬희질문사항
-            var a = blocks[i].style.left;
-            a = Number(a.replace(/px$/, ''));
-            console.log(a);
-            a += 450;
-            console.log(a);
-            blocks[i].style.left = a; 
-        }       
+        blocks[i].style.left += 170;                           
     }
 }
 
@@ -21,28 +11,39 @@ function keyArrowLeft(){
     console.log("Left Pressed.");
     let blocks = document.getElementsByClassName("blockPrefab");
     for (let i = 0; i < blocks.length; i++){
-        if(blocks[i].style.left == 470)
-            blocks[i].style.left -= 450;        
+        blocks[i].style.left -= 170;        
     }
 }
 function keyArrowUp(){
     console.log("Up Pressed.");
     let blocks = document.getElementsByClassName("blockPrefab");
     for (let i = 0; i < blocks.length; i++){
-        blocks[i].style.top = 20;        
+        blocks[i].style.top -= 170;        
     }
 }
 function keyArrowDown(){
     console.log("Down Pressed.");
     let blocks = document.getElementsByClassName("blockPrefab");
     for (let i = 0; i < blocks.length; i++){
-        blocks[i].style.top = 470;        
+        blocks[i].style.top += 170;        
     }
+}
+
+function gameStart(){
+    var blockmap = new BlockMap();
+    blockmap.addblock(new Block(2));
+
+    
+    document.createElement("div");
+    div_app.appendChild();
 }
 
 function keylog(e){
     console.log(e.key);
     switch(e.key){
+        case 'Enter':
+            gameStart();
+            break;
         case 'ArrowRight':
             keyArrowRight();
             break;
@@ -55,9 +56,56 @@ function keylog(e){
         case 'ArrowDown':
             keyArrowDown();
             break;
-        default:
+        default:            
             break;
     }
 }
 
 window.onkeydown = keylog;
+
+class BlockMap{
+    constructor(row,column){
+        this.Map = Array.from(Array(row), ()=> Array(column).fill(0));
+        this.div_app = document.getElementById("app");
+    }
+    addblock(x,y,block){
+        block.setactive(true);
+        block.setcoord(x,y);
+        this.Map[x][y] = block;
+    }
+    removeblock(x,y){
+        block.setactive(false);
+        this.Map[x][y] = null;
+    } 
+
+}
+
+class Block{
+    constructor(value){
+        this.Value = value;
+        this.Active = false;
+        this.XPos = 20;
+        this.YPos = 20;
+        this.Element = document.createElement("div");
+        this.Element.setAttribute("class","blockPrefab")
+    }
+    changevalue(value){
+        this.Value = value;
+    }
+    setactive(isActivated){
+        this.Active = isActivated;        
+    }
+    setcoord(x,y){
+        this.XPos += 150*x;
+        this.YPos += 150*y;
+    }
+    removeblock(){
+        this.Active = false;
+        this.XPos = 20;
+        this.YPos = 20;
+    }
+
+}
+
+blockMap[0][0] = new Block(2);
+blockMap[0][3] = new Block(2);
