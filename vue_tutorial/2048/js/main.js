748f2289ea5eb318c3203f3869cbeedad92deede
elflee77 <elflee77@gmail.com>
2022-12-06 11:36:26 +0900

    okay

--- vue_tutorial/2048/js/main.js
+++ vue_tutorial/2048/js/main.js
@@ -1,52 +1,20 @@
 class BlockMap{
     constructor(row,column){
-        this.Row = row;
-        this.Column = column;
         this.Map = Array.from(Array(row), ()=> Array(column).fill(0));
-        this.div_app = document.getElementById("app");        
+        this.div_app = document.getElementById("app");
     }
     addblock(x,y,block){
         block.setactive(true);
         block.setcoord(x,y);
         this.Map[x][y] = block;
-        block.setcoord(x,y);
-        
-        this.div_app.appendChild(block.div_element)
-    }
-    addrandomblock(block){
-        block.setactive(true);
-        
-        
-        //랜덤 블록 위치 생성
-
-
-        block.setcoord(x,y);
-        this.Map[x][y] = block;
-        this.div_app.appendChild(block.div_element)
+        this.div_app.appendChild(block.Element);
+        console.log(block.XPos,block.YPos);
     }
     removeblock(x,y){
         block.setactive(false);
         this.Map[x][y] = null;
-    }
-    moveleft(){
-        
-    }
-    moveright(){
-
-    }
-    moveup(){
-
-    }
-    movedown(){
-
     } 
 
-    draw(){
-        //for(let i=0;i<this.Row;i++)
-        //    for(let j=0;j<this.Column;j++)
-        
-        //this.Map[x][y]
-    }
 }
 
 class Block{
@@ -55,30 +23,32 @@ class Block{
         this.Active = false;
         this.XPos = 20;
         this.YPos = 20;
-        this.div_element = document.createElement("div");
-        this.div_element.setAttribute("class","blockPrefab")
-        this.div_element.appendChild(document.createTextNode("2"));
+        this.Element = document.createElement("div");
+        this.Element.setAttribute("class","blockPrefab");
+        this.Element.append(this.Value);
     }
     changevalue(value){
         this.Value = value;
     }
     setactive(isActivated){
+        this.Element.style.display = "block";
         this.Active = isActivated;        
     }
     setcoord(x,y){
         this.XPos += 150*x;
         this.YPos += 150*y;
-        this.div_element.style.left = this.XPos;
-        this.div_element.style.top = this.YPos;
     }
     removeblock(){
+        this.Element.style.display = "none"
         this.Active = false;
         this.XPos = 20;
         this.YPos = 20;
     }
-
 }
-/*
+
+window.onkeydown = keylog;
+var blockmap = new BlockMap(4,4);
+
 function keyArrowRight(){
     console.log("Right Pressed.");
     let blocks = document.getElementsByClassName("blockPrefab");
@@ -108,37 +78,38 @@ function keyArrowDown(){
     for (let i = 0; i < blocks.length; i++){
         blocks[i].style.top += 170;        
     }
-}*/
+}
 
-var blockmap = new BlockMap(4,4);
+function KeyEnter(){
+    
+    blockmap.addblock(1,1, new Block(2));
 
-function gameStart(){    
-    blockmap.addblock(Math.floor(Math.random() * 4),Math.floor(Math.random() * 4),new Block(2));
 }
 
 function keylog(e){
     console.log(e.key);
     switch(e.key){
         case 'Enter':
-            gameStart();
+            KeyEnter();
             break;
         case 'ArrowRight':
-            blockmap.moveright();
+            keyArrowRight();
             break;
         case 'ArrowLeft':
-            blockmap.moveleft();
+            keyArrowLeft();
             break;
         case 'ArrowUp':
-            blockmap.moveup();
+            keyArrowUp();
             break;
         case 'ArrowDown':
-            blockmap.movedown();
+            keyArrowDown();
             break;
         default:            
             break;
     }
 }
 
-window.onkeydown = keylog;
+
+
 
 
